9d4b1ea08aff325c34b97f796529f73419e5306b
Tsyren Ochirov <nsu1team@gmail.com>
2014-09-16 21:01:05 +0200

    S3

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,9 +13,13 @@ Spree::Api::Config.requires_authentication = false
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
+  config.use_s3 = true
+  config.s3_bucket = 'shoppinginheroku'
+  config.s3_access_key = "tsyren.hey"
+  config.s3_secret = "J0s120o78669Jy9"
+  config.attachment_url = ":s3_eu_url"
+  config.s3_host_alias = "s3-eu-west-1.amazonaws.com"
+
   config.allow_guest_checkout = true
 end
 
@@ -23,3 +27,7 @@ Spree.user_class = "Spree::User"
 Rails.application.config.spree.payment_methods << Spree::PaymentMethod::Omnikassa
 
 Spree::Image.attachment_definitions[:attachment][:styles] = { mini: '69x98>', small: '190x290>', product: '311x467">', large: '600x600>' }
+
+Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+"#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+end
