df9542d9920757ec5b942e1a787f73e70964186f
Tsyren Ochirov <nsu1team@gmail.com>
2014-09-28 18:01:20 +0200

    Sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def taxons_tree_with_selector(root_taxon, current_taxon)
+    max_level = 1 
+
+    return '' if root_taxon.children.empty?
+    content_tag :ul, class: 'taxons-list' do
+      root_taxon.children.map do |taxon|
+        content_tag :li do
+         check_box_tag(taxon.name, :value => 1, :id => "taxon_#{taxon.id}", :class => 'css-checkbox') +
+         label_tag("taxon_#{taxon.id}")
+        end
+      end.join("\n").html_safe
+    end
+  end
 end
