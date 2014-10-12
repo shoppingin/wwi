172eda71c36811d1d52b714129407a69ceedf2be
Tsyren Ochirov <nsu1team@gmail.com>
2014-10-12 21:02:01 +0200

    Makeup fix.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def taxons_tree_with_selector(root_taxon, current_taxon)
-    max_level = 1 
+    max_level = 1
 
     return '' if root_taxon.children.empty?
     content_tag :ul, class: 'taxons-list' do
@@ -10,6 +10,17 @@ module ApplicationHelper
          label_tag("taxon_#{taxon.id}")
         end
       end.join("\n").html_safe
+    end
+  end
+
+  def taxonomy_name(name)
+    case name
+    when 'Brand'
+       'MERKEN'
+    when 'Categories'
+       'CATEGORIE'
+    else
+      name
     end
   end
 end
