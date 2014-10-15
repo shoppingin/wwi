e942cb6cb21607abc7a44149150bede668de5f49
Tsyren Ochirov <nsu1team@gmail.com>
2014-10-15 17:17:20 +0200

    Fix spell mistake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: ENV['APP_HOST'] }
-  config.action_mailer.devilery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
